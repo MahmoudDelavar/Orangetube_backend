defd9a66cc254e226660e2b4dad234782a5b5b40
mahmoud <mahmoud.d.p7@gmail.com>
2022-06-22 00:17:52 -0700

    edit priduct model

--- src/models/product.js
+++ src/models/product.js
@@ -5,10 +5,10 @@ const timestamp = require("mongoose-timestamp");
 const productSchema = new mongoose.Schema({
   title: { type: String, required: true },
   writer: { type: String, required: true },
-  explan: { type: String, required: true },
+  // explan: { type: String, required: true },
   pric: { type: Number, required: true },
   count: { type: Number, required: true },
-  category: { type: String, required: true },
+  // category: { type: String, required: true },
 });
 productSchema.plugin(timestamp);
 module.exports = mongoose.model("Product", productSchema);
