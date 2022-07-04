18fdc642a3bf7e53b093e07ef088aa50684807a8
mahmoud <mahmoud.d.p7@gmail.com>
2022-07-04 05:33:28 -0700

    set filter to loadUsrs

--- src/routes/loadUsers/controller.js
+++ src/routes/loadUsers/controller.js
@@ -5,7 +5,10 @@ const controller = require("./../controller");
 module.exports = new (class extends controller {
   //----------lode Users Controller-----
   async loadUsers(req, res) {
-    let user = await this.User.find().exec();
+    let user = await this.User.find()
+      .select({ name: 1, email: 1, createdAt: 1 })
+      .sort({ createdAt: 1 })
+      .exec();
     this.response({ res, code: 200, message: "loaded All Users", data: user });
   }
 })();
